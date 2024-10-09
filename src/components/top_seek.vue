a2378d22a36b39d52b053081a642106a84b1165c
zzx550 <zzx550@proton.me>
2024-10-09 22:29:29 +0800

    1
    1

--- src/components/top_seek.vue
+++ src/components/top_seek.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="right" v-if="isLogin == true">
       <div>
-        <a-badge :count="num" @click="router.push('./news')">
+        <a-badge :count="notReadMsgNum" @click="router.push('./news')">
           <img class="ts" src="../assets/home/sy.png" />
         </a-badge>
       </div>
@@ -67,199 +67,190 @@
 </template>
 
 <script setup lang="ts">
-  import { useStore } from 'vuex'
-  const { state } = useStore()
-  import { api_logout, api_problemFeedback, api_newsList } from '@/requset/api'
-  import router from '@/router'
-  import { ref, defineProps, watch, defineEmits } from 'vue'
-  import { message, type MenuProps } from 'ant-design-vue'
+import { useStore } from "vuex";
+const { state } = useStore();
+import { api_logout, api_problemFeedback, api_getInfo } from "@/requset/api";
+import router from "@/router";
+import { ref, defineProps, watch, defineEmits } from "vue";
+import { message, type MenuProps } from "ant-design-vue";
 
-  const emit = defineEmits(['changeKey'])
+const emit = defineEmits(["changeKey"]);
 
-  // 基本写法
-  const props = defineProps({
-    title: {
-      type: String,
-      require: false,
-    },
-  })
+// 基本写法
+const props = defineProps({
+  title: {
+    type: String,
+    require: false,
+  },
+});
 
-  const seekValue = ref<string>('')
-  const modal_fk = ref<boolean>(false)
-  const feebackContent = ref<string>('')
-  let isLogin = ref<boolean>(false)
-  let noticeList = ref<any>([])
+const seekValue = ref<string>("");
+const modal_fk = ref<boolean>(false);
+const feebackContent = ref<string>("");
+let isLogin = ref<boolean>(false);
+let notReadMsgNum = ref<number>(0);
 
-  isLogin.value =
-    sessionStorage.getItem('token') != null &&
-    sessionStorage.getItem('token') != '' &&
-    sessionStorage.getItem('token') != undefined
+isLogin.value =
+  sessionStorage.getItem("token") != null &&
+  sessionStorage.getItem("token") != "" &&
+  sessionStorage.getItem("token") != undefined;
 
-  function sou() {
-    router.push('/shop?value=' + seekValue.value)
-  }
+function sou() {
+  router.push("/shop?value=" + seekValue.value);
+}
 
-  api_newsList({ type: 1 }).then((res: any) => {
+if (isLogin.value == true) {
+  api_getInfo({}).then((res: any) => {
+    notReadMsgNum.value = res.data.notReadMsgNum
+      ? Number(res.data.notReadMsgNum)
+      : 0;
+  });
+}
+
+const onClick: MenuProps["onClick"] = ({ key }) => {
+  state.countryLang.forEach((x: any, y: number) => {
+    if (x.lang == key) {
+      sessionStorage.setItem("lang", String(key));
+      sessionStorage.setItem("lang_w", x.value);
+      router.go(0);
+    }
+  });
+};
+
+const feedBackSubmit = () => {
+  if (feebackContent.value == "") return;
+  api_problemFeedback({
+    content: feebackContent.value,
+  }).then((res: any) => {
+    if (res.code == 200) {
+      modal_fk.value = false;
+      message.success("提交成功");
+      feebackContent.value = "";
+    } else if (res.message) {
+      message.error(res.message);
+    }
+  });
+};
+
+const loginOut = () => {
+  api_logout({}).then((res: any) => {
     if (res.success) {
-      noticeList.value.push(res.data)
+      sessionStorage.removeItem("token");
+      router.push("./");
     }
-  })
-  api_newsList({ type: 2 }).then((res: any) => {
-    if (res.success) {
-      noticeList.value.push(res.data)
-    }
-  })
-  let num = ref<number>(0)
-  noticeList.value.forEach((x: any) => {
-    if (x.is_read != 1) {
-      num.value++
-    }
-  })
-
-  const onClick: MenuProps['onClick'] = ({ key }) => {
-    state.countryLang.forEach((x: any, y: number) => {
-      if (x.lang == key) {
-        sessionStorage.setItem('lang', String(key))
-        sessionStorage.setItem('lang_w', x.value)
-        router.go(0)
-      }
-    })
-  }
-
-  const feedBackSubmit = () => {
-    if (feebackContent.value == '') return
-    api_problemFeedback({
-      content: feebackContent.value,
-    }).then((res: any) => {
-      if (res.code == 200) {
-        modal_fk.value = false
-        message.success('提交成功')
-        feebackContent.value = ''
-      } else if (res.message) {
-        message.error(res.message)
-      }
-    })
-  }
-
-  const loginOut = () => {
-    api_logout({}).then((res: any) => {
-      if (res.success) {
-        sessionStorage.removeItem('token')
-        router.push('./')
-      }
-    })
-  }
+  });
+};
 </script>
 
 <style scoped lang="less">
-  .top_ {
-    height: 60px;
+.top_ {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+  .logo {
+    height: 43px;
+  }
+  .seek {
+    margin: 0 30px;
+    flex: 1;
+    height: 46px;
+    position: relative;
+    input {
+      width: 100%;
+      height: 100%;
+      border-radius: 8px;
+      border: 1px solid #707070;
+      background-color: transparent;
+      font-size: 16px;
+      &::placeholder {
+        color: #1d1e25;
+        opacity: 30%;
+      }
+    }
+    .icon {
+      position: absolute;
+      right: 20px;
+      top: 50%;
+      transform: translateY(-50%);
+      height: 24px;
+      width: 24px;
+    }
+  }
+  .right {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: 30px;
-    .logo {
-      height: 43px;
+    font-size: 20px;
+    font-weight: 700;
+    padding: 10px;
+    color: #0ae2db;
+    img {
+      width: 32px;
+      height: 32px;
     }
-    .seek {
-      margin: 0 30px;
-      flex: 1;
-      height: 46px;
-      position: relative;
-      input {
-        width: 100%;
-        height: 100%;
-        border-radius: 8px;
-        border: 1px solid #707070;
-        background-color: transparent;
-        font-size: 16px;
-        &::placeholder {
-          color: #1d1e25;
-          opacity: 30%;
-        }
-      }
-      .icon {
-        position: absolute;
-        right: 20px;
-        top: 50%;
-        transform: translateY(-50%);
-        height: 24px;
-        width: 24px;
-      }
-    }
-    .right {
+    .shop {
       display: flex;
-      justify-content: space-between;
       align-items: center;
-      font-size: 20px;
-      font-weight: 700;
-      padding: 10px;
-      color: #0ae2db;
-      img {
-        width: 32px;
-        height: 32px;
-      }
-      .shop {
-        display: flex;
-        align-items: center;
-        height: 38px;
-        background-color: #0ae2db;
-        border-radius: 4px;
-        padding: 0 10px;
-        font-size: 14px;
-        color: #fff;
-        img {
-          width: 24px;
-          height: 24px;
-          margin-right: 10px;
-        }
-      }
-    }
-  }
-  .two {
-    align-items: center;
-    .seek {
-      height: auto;
-      font-size: 20px;
-      font-weight: 600;
-    }
-    .right {
-      .fk_ {
-        margin-right: 20px;
-        display: flex;
-        align-items: center;
-        img {
-          width: 20px;
-          height: 20px;
-          margin-right: 10px;
-        }
-      }
-    }
-  }
-  .modal_wit {
-    .title {
-      font-size: 18px;
-      margin-bottom: 20px;
-      font-weight: 600;
-    }
-    .txt {
-      width: 513px;
-      height: 317px;
-      background-color: #f6f7f9;
-      border-radius: 8px;
-      border: none;
-      padding: 5px;
-      font-size: 16px;
-      margin-bottom: 40px;
-    }
-    .but {
-      width: 150px;
-      padding: 8px 10px;
-      border-radius: 4px;
+      height: 38px;
       background-color: #0ae2db;
-      text-align: center;
-      color: #fff;
+      border-radius: 4px;
+      padding: 0 10px;
       font-size: 14px;
+      color: #fff;
+      img {
+        width: 24px;
+        height: 24px;
+        margin-right: 10px;
+      }
     }
   }
+}
+.two {
+  align-items: center;
+  .seek {
+    height: auto;
+    font-size: 20px;
+    font-weight: 600;
+  }
+  .right {
+    .fk_ {
+      margin-right: 20px;
+      display: flex;
+      align-items: center;
+      img {
+        width: 20px;
+        height: 20px;
+        margin-right: 10px;
+      }
+    }
+  }
+}
+.modal_wit {
+  .title {
+    font-size: 18px;
+    margin-bottom: 20px;
+    font-weight: 600;
+  }
+  .txt {
+    width: 513px;
+    height: 317px;
+    background-color: #f6f7f9;
+    border-radius: 8px;
+    border: none;
+    padding: 5px;
+    font-size: 16px;
+    margin-bottom: 40px;
+  }
+  .but {
+    width: 150px;
+    padding: 8px 10px;
+    border-radius: 4px;
+    background-color: #0ae2db;
+    text-align: center;
+    color: #fff;
+    font-size: 14px;
+  }
+}
 </style>
