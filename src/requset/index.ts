8c867a0d41a57d4cff1f9c09a4e726d1469eae7e
Xiaoma <111112486+zzx550@users.noreply.github.com>
2024-10-22 22:36:28 +0800

    1

--- src/requset/index.ts
+++ src/requset/index.ts
@@ -2,12 +2,8 @@ import Request from "./http";
 
 const request: Request = new Request({
   // 公共前缀
-  // G站
-  // baseURL: 'https://tkg-api.zale.mobi/api/',
   // 测试A站
-  // baseURL: "https://tka-api.zale.mobi/api/",
-  // K站
-  baseURL: "https://tkk-api.zale.mobi/api/",
+  baseURL: "https://tka-api.zale.mobi/api/",
   // 请求头
   headers: {
     Accept: "application/json",
