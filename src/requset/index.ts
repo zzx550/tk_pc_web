ffed9b02ebe29e87cd38afe6fc2cf36f2e5b789f
zzx550 <zzx550@proton.me>
2024-12-16 23:53:33 +0800

    换域名

--- src/requset/index.ts
+++ src/requset/index.ts
@@ -3,7 +3,7 @@ import Request from "./http";
 const request: Request = new Request({
   // 公共前缀
   // G站
-  baseURL: " https://g-to.icu/api/",
+  baseURL: " https://g-shop.bond/api/",
   // baseURL: ' https://www.mysshopvip.shop/api/',
   // 请求头
   headers: {
