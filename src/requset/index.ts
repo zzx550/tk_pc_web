2d0f0b0a4d81b487289f6278596ca8003f28046d
zzx550 <zzx550@proton.me>
2025-01-28 17:10:31 +0800

    1
    1

--- src/requset/index.ts
+++ src/requset/index.ts
@@ -3,7 +3,7 @@ import Request from "./http";
 const request: Request = new Request({
   // 公共前缀
   // G站
-  baseURL: " https://g-shop.bond/api/",
+  baseURL: " https://g-shop.cyou/api/",
   // baseURL: ' https://www.mysshopvip.shop/api/',
   // 请求头
   headers: {
